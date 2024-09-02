12ef0f6ff4dcaf02c05ad185d0f40ea2d3be7412
Pradip Kumar Murmu <trexperiment2050@gmail.com>
2024-09-02 16:08:09 +0530

    Making a call to the Ratings API

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogueResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogueResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @RequestMapping("/catalog")
@@ -25,10 +26,15 @@ public class MovieCatalogueResource {
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	@Autowired
+	private WebClient.Builder webClientBuilder;
+	
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 		
 		//RestTemplate restTemplate = new RestTemplate();
+		//Webclients
+		//WebClient.Builder builder = WebClient.builder();
 		
 		//get all rated movie IDs
 		List<Rating> ratings =Arrays.asList(
@@ -39,8 +45,16 @@ public class MovieCatalogueResource {
 		return ratings.stream()
 		.map(rating -> {
 		//new CatalogItem("Transforms","Test",4))
-		Movie movie = restTemplate.getForObject("http://localhost:8082/movie/"+rating.getMovieId(), Movie.class);
-		return new CatalogItem(movie.getName(),"Test",rating.getRating());
+		//Movie movie = restTemplate.getForObject("http://localhost:8082/movie/"+rating.getMovieId(), Movie.class);
+		//return new CatalogItem(movie.getName(),"Test",rating.getRating());
+		
+		Movie movie = webClientBuilder.build()
+						.get()
+						.uri("http://localhost:8082/movie/"+rating.getMovieId())
+						.retrieve()
+						.bodyToMono(Movie.class)
+						.block();
+		return new CatalogItem(movie.getName(),"Desc", rating.getRating());
 		})
 		.collect(Collectors.toList());
 		// for each movie ID, CALL MVIE INFOR SERVICE AND GET DETAILS 
--- ratings-data-service/src/main/java/io/javabrains/ratingdataservice/resources/RatingResource.java
+++ ratings-data-service/src/main/java/io/javabrains/ratingdataservice/resources/RatingResource.java
@@ -1,5 +1,8 @@
 package io.javabrains.ratingdataservice.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +16,17 @@ public class RatingResource {
 	@RequestMapping("/{movieId}")
 	public Rating getRating(@PathVariable("movieId") String movieId) {
 		return new Rating(movieId, 4);
+		
+	
 	}
+	
+	@RequestMapping("/users/{userId}")
+	public List<Rating> getUserRating(@PathVariable("userId") String userId){
+		List<Rating> ratings =Arrays.asList(
+				new Rating("1234",4),
+				new Rating("5678", 5)
+		);
+		return ratings;
+	}
+	
 }
