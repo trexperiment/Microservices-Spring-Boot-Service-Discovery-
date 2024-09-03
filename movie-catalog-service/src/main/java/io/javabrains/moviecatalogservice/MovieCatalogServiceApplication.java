4ef7c16ca2ff8c256470585949b1489eedb5cc1a
Pradip Kumar Murmu <trexperiment2050@gmail.com>
2024-09-03 13:18:08 +0530

    Introducing Eureka Server, Starting Eureka server i.e. discovery-server, Creating Eureka clients i.e. movie-info-service, ratings-data-service, movie-catalog-service

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,6 +2,7 @@ package io.javabrains.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
